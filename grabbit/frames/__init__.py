efe4073bb088a547cfb281e76ea51db19be6c5e4
Mike Lang <mikelang3000@gmail.com>
2014-04-13 21:43:25 -0700

    Fix a typo in frame.__init__

--- grabbit/frames/__init__.py
+++ grabbit/frames/__init__.py
@@ -2,6 +2,6 @@ from common import Incomplete
 from fieldtable import FieldTable
 from frame import Frame
 from method import Method
-from properties import Property, PropertyBit
+from properties import Properties, PropertyBit
 
 from datatypes import DataType, Octet, Short, Long, LongLong, ShortString, LongString, Bits, Sequence
