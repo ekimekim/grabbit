52c77102aec759c50b2ea28b8e68f52f40cf39f4
Mike Lang <mikelang3000@gmail.com>
2014-04-13 22:31:00 -0700

    frames.method: Add a common "response" attr that may contain response method

--- grabbit/frames/method.py
+++ grabbit/frames/method.py
@@ -3,7 +3,12 @@ from datatypes import Sequence
 
 class Method(Sequence):
 	"""Subclass this class to define a method of the given method_class and method_id.
-	Don't forget to define fields as per datatype.Sequence"""
+	Optional attribute "response" may define the synchronous response method for a given method
+		(for example, OpenOk is the response to Open). It should be left as None for methods
+		without a synchronous response.
+	Don't forget to define fields as per datatype.Sequence
+	"""
 	method_class = NotImplemented
 	method_id = NotImplemented
+	response = None
 
