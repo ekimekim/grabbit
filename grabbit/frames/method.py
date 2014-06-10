51b2cc4517e8983806064be95cc51096b426ca4f
Mike Lang <mikelang3000@gmail.com>
2014-06-10 05:43:30 -0700

    frames.method: Introduce concept of "does this method expect content following it"
    This generalised approach will help later with associating incoming methods with their content.

--- grabbit/frames/method.py
+++ grabbit/frames/method.py
@@ -6,9 +6,12 @@ class Method(Sequence):
 	Optional attribute "response" may define the synchronous response method for a given method
 		(for example, OpenOk is the response to Open). It should be left as None for methods
 		without a synchronous response.
+	Attribute "has_content" indicates that the method is to be followed by content frames,
+		and defaults to False.
 	Don't forget to define fields as per datatype.Sequence
 	"""
 	method_class = NotImplemented
 	method_id = NotImplemented
 	response = None
+	has_content = False
 
