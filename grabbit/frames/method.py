2c4c101757ed631ae0481c8bb132a387652c05db
Mike Lang <mikelang3000@gmail.com>
2014-06-10 08:08:47 -0700

    frames.method: Allow multiple response Methods from a Method
    This is needed for basic.Get -> basic.GetOk, basic.GetEmpty

--- grabbit/frames/method.py
+++ grabbit/frames/method.py
@@ -3,9 +3,9 @@ from datatypes import Sequence
 
 class Method(Sequence):
 	"""Subclass this class to define a method of the given method_class and method_id.
-	Optional attribute "response" may define the synchronous response method for a given method
+	Optional attribute "response" may define the synchronous response method(s) for a given method
 		(for example, OpenOk is the response to Open). It should be left as None for methods
-		without a synchronous response.
+		without a synchronous response. Use any iterable for multiple acceptable methods.
 	Attribute "has_content" indicates that the method is to be followed by content frames,
 		and defaults to False.
 	Don't forget to define fields as per datatype.Sequence
