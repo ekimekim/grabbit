6dccbccf3126a7469f8635c98b01652caff9b5ac
Mike Lang <mikelang3000@gmail.com>
2014-06-10 05:38:32 -0700

    Fix some bad indendation in frames.common

--- grabbit/frames/common.py
+++ grabbit/frames/common.py
@@ -5,8 +5,8 @@ class Incomplete(Exception):
 
 
 def eat(data, length):
-       """Helper method: Split (length) bytes from data and return it along with remainder,
-       or raise Incomplete if not long enough."""
-       if len(data) < length:
-               raise Incomplete
-       return data[:length], data[length:]
+	"""Helper method: Split (length) bytes from data and return it along with remainder,
+	or raise Incomplete if not long enough."""
+	if len(data) < length:
+		raise Incomplete
+	return data[:length], data[length:]
