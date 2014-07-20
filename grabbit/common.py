5a88306ef9e6b9cf63c7cc6911bd14dbe6e613eb
Mike Lang <mikelang3000@gmail.com>
2014-07-20 09:45:41 -0700

    common: Implement classproperty
    A classproperty is a decorator that acts like @property but the called method is a classmethod.
    This allows properties to work from the class level, ie. MyClass.computed_value

--- grabbit/common.py
+++ grabbit/common.py
@@ -8,3 +8,14 @@ def get_all_subclasses(cls):
 	subs = set(cls.__subclasses__())
 	subs_of_subs = [get_all_subclasses(subcls) for subcls in subs]
 	return subs.union(*subs_of_subs)
+
+
+class classproperty(object):
+	"""Acts like a stdlib @property, but the wrapped get function is a class method.
+	For simplicity, only read-only properties are implemented."""
+
+	def __init__(self, fn):
+		self.fn = fn
+
+	def __get__(self, instance, cls):
+		return self.fn(cls)
