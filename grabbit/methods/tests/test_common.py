e5eb5101411cee15990f0a94e5da8ab2df690ae2
Mike Lang <mikelang3000@gmail.com>
2014-10-21 04:30:19 +0000

    methods.tests.test_common: Some docstrings

--- grabbit/methods/tests/test_common.py
+++ grabbit/methods/tests/test_common.py
@@ -24,6 +24,8 @@ class TestError(AMQPError):
 class CloseMethodTests(TestCase):
 
 	def construct_full(self, error=True, method=True):
+		"""Shortcut to construct a CloseMethod using the full code, reason, method_class, method_id
+		arguments."""
 		code, reason, method_class, method_id = 0, '', 0, 0
 		if error:
 			code, reason = TestError.code, TestError.__doc__
@@ -32,10 +34,13 @@ class CloseMethodTests(TestCase):
 		return TestCloseMethod(code, reason, method_class, method_id)
 
 	def construct(self, error=True, method=True):
+		"""Shortcut to construct a CloseMethod using the helper error and method arguments."""
 		return TestCloseMethod(error = TestError if error else None,
 		                       method = TestBadMethod if method else None)
 
 	def test_equality(self):
+		"""Test all combinations of having and not having an error and a method,
+		and ensure both constructors return equal objects."""
 		for error, method in itertools.product([True, False], repeat=2):
 			self.assertEqual(self.construct_full(error, method),
 			                 self.construct(error, method))
